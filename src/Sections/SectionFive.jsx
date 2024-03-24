a67e57585dbc4150bfcc5f9427531a728ee555b6
StudenxFinance <studenxfinance@gmail.com>
2024-03-24 21:21:28 +0200

    added section

--- src/Sections/SectionFive.jsx
+++ src/Sections/SectionFive.jsx
@@ -3,7 +3,17 @@ import React from "react";
 export const SectionFive = () => {
     return (
         <section className="section five" >
-            
+            <div className="five-content" >
+                <div className="five-content-text" >
+                    <h1 className="five-main" >Exceteur Magna</h1>
+                    <p className="five-description" >
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                </div>
+                <div className="five-right" >
+                    <div className="five-data" ></div>
+                </div>
+            </div>
         </section>
     )
 }
--- src/Sections/SectionSix.jsx
+++ src/Sections/SectionSix.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const SectionSix = () => {
     return (
-        <section>
+        <section className="section six" >
             
         </section>
     )
